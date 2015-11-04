4d9dc0f93a2eb6c6549616efd04de17a9f198c53
Sayudha Lukita Wibisana <sayudhagit@hotmail.com>
2015-11-04 07:11:51 +0700

    Learning Replace
    Learn to put replace syntax in my resume builder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,4 @@
 var awesomeThoughts = "I am Sayudha and I am AWESOME!";
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+//lesson1 - Belajar Replace -
 $("#main").append([funThoughts]);
